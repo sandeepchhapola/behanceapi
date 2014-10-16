cdb529afdc948027e5dfdd1187d6fb57a4bd3473
sandeepchhapola <schhapola0001@gmail.com>
2014-10-16 22:47:02 +0530

    minnor changes

--- behanceapi.js
+++ behanceapi.js
@@ -1,5 +1,5 @@
 var behance = require('./lib/behance');
 
-expotrs.behanceapi = function (clientId, ClientSecret) {
-    return new behance.Behance(clientId, ClientSecret);
+expotrs.behanceapi = function (clientId) {
+    return new behance.Behance(clientId);
 };
